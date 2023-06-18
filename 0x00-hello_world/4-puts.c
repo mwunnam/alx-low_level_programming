911f5daea9012149288797633abe7623c28862b2
mwunnam <mwunnam@gmail.com>
2023-06-18 17:27:35 +0000

    Description and * possition correction"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
-{	char* message = "Programming is like building a multilingual puzzle\n";
+{	char *message = "Programming is like building a multilingual puzzle\n";
 	puts(message);
 	return (0);
 }
