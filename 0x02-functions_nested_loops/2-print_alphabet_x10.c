ea5d01000e73115462a61eac961f8a55310bfb90
mwunnam <mwunnam@gmail.com>
2023-06-21 23:32:20 +0000

    2_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
-*
-* Return: 0 (success)
-*
-*/
-int main(void)
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: 0 (success)
+ *
+ */
+
+
+void print_alphabet_x10(void)
 {
-return (0);
+	int num;
+
+	for (num = 1; num <= 10; num++)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
 }
