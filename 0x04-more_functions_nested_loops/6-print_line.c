f0415e235b957b8c80feef24509c47dcc3f6ffde
mwunnam <mwunnam@gmail.com>
2023-06-22 23:29:03 +0000

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,10 +13,13 @@ void print_line(int n)
 	int i;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+		return;
+	}
 
-	else
-		for (i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 		_putchar('_');
+
 	_putchar('\n');
 }
