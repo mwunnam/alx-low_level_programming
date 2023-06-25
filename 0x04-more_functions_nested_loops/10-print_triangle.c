935feb09d1790dc330fd43ec57dcd303c351f8ed
mwunnam <mwunnam@gmail.com>
2023-06-26 00:26:22 +0100

    triangle done

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,12 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
+* print_triangle - Entry point
+*
+* @size: the size of the triangle
 *
 * Return: 0 (success)
 *
 */
-int main(void)
+void print_triangle(int size)
 {
-return (0);
+	int i;
+
+	int j;
+	
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (i + j <= size)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
