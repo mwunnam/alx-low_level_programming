cd8020cb231b94778edee02d5689f04114f65aa0
mwunnam <mwunnam@gmail.com>
2023-06-28 00:10:33 +0100

    done with 0 task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98(int *n) : Entry point
+ *
+ * @int *n: the parameter to put in
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
