2006a655b2f8f5cd73a03803fd978d7e4698199f
Michael Wunnam Issahaku <mwunnam@gmail.com>
2023-06-28 14:09:16 -0700

    done with 3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ *
+ * @str: string to b printed
+ *
+ * Return : Always 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
