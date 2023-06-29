f1e1d79904b6784c7ae5129fbf15eee4e01c32ae
mwunnam <mwunnam@gmail.com>
2023-06-30 00:39:09 +0100

    done with 1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,10 +2,33 @@
 #include <stdio.h>
 
 /**
- * function_name - 
+ * _strncat - concatenate strings
  *
- * @: the parameter to be inputed
+ * @dest: input parameter
+ * @src: input parameter
+ * @n: input parameter
  *
- * Return: 0 Always
+ * Return: dest
  */
 
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int k;
+
+	i = 0;
+	k = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (k < n && src[k] != '\0')
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+
+	}
+	dest[i] = '\0';
+	return (dest);
+}
