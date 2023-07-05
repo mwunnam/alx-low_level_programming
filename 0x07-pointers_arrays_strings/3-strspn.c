2059527e9bf83700020135f46c95d89702bce4b0
Michael Wunnam Issahaku <mwunnam@gmail.com>
2023-07-04 18:37:44 -0700

    done with 3-strpn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ *
+ * @s: input parameter
+ * @accept: input parameter
+ *
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[r]; i++)
+		{
+			if (*s == accept[i])
+			{
+				n++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
+
