4c58d4117685fb55599e33521d7c7eed828ea214
Michael Wunnam Issahaku <mwunnam@gmail.com>
2023-07-04 18:47:00 -0700

    did from 3-strspn.c 5-strstr.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,27 +2,27 @@
 
 /**
  * _strspn - Entry point
- *
  * @s: input parameter
  * @accept: input parameter
  *
  * Return: Always 0 (Success)
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-	int i;
+	int r;
 
 	while (*s)
 	{
-		for (i = 0; accept[r]; i++)
+		for (r = 0; accept[r]; r++)
 		{
-			if (*s == accept[i])
+			if (*s == accept[r])
 			{
 				n++;
 				break;
 			}
-			else if (accept[i + 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
