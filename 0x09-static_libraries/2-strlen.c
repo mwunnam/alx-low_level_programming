1955a4b9af2ccfc75794d5d41664438adc8b1ac3
mwunnam <mwunnam@gmail.com>
2023-07-09 01:36:57 +0100

    defined function _strlen

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
- * strlen - function that print on the standard output
+ * _strlen - function that print on the standard output
  *
  * @s: parameter to be entered
  *
  * Return: 0 when successsfull
  */
 
-int _strlen(char *s);
+int _strlen(char *s)
+{
+
+
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+
+}
