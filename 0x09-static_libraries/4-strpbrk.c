c62c53c119904507a1b82b49231412325ed2254a
mwunnam <mwunnam@gmail.com>
2023-07-10 11:32:11 +0000

    done with 5-strstr.c and 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -9,4 +9,14 @@
  * Return: 0 when successsfull
  */
 
-char *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept)
+{	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+			if (*s == accept[k])
+			return (s);
+	}
+	return ('\0');
+}
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -9,4 +9,20 @@
  * Return: 0 when successsfull
  */
 
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
+{
+	for ( ; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *p = needle;
+
+		while (*i == *p && *p != '\0')
+		{
+			i++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
