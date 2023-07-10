fdf964aa6cc550d2919f63b38b7191f5fa8be5d4
mwunnam <mwunnam@gmail.com>
2023-07-10 14:18:22 +0000

    doen with 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,4 +8,33 @@
  * Return: 0 when successsfull
  */
 
-int _atoi(char *s);
+int _atoi(char *s)
+{
+	int sign;
+
+	int i;
+
+	unsigned int res;
+
+	i = 0;
+
+	sign = 1;
+
+	res = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+			i++;
+	}
+
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+
+	return (res);
+}
