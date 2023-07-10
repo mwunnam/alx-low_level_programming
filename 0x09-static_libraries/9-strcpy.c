a6e869a4ee7e717d171445dff626999cbc000647
mwunnam <mwunnam@gmail.com>
2023-07-10 13:13:51 +0000

    done with 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -9,4 +9,24 @@
  * Return: 0 when successsfull
  */
 
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	int b;
+
+	a = 0;
+
+	b = 0;
+
+	while (*(src + a) != '\0')
+		a++;
+
+	for ( ; b < a; b++)
+	dest[b] = src[b];
+
+
+	dest[a] = '\0';
+
+	return (dest);
+}
