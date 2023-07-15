44714341459e3171e914aee5732144d1124e8269
mwunnam <mwunnam@gmail.com>
2023-07-15 14:18:07 +0000

    done with task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion : prints the lenght of a sting
+ * _strlen_recursion - prints the lenght of a sting
  *
  * @s: parameter for hold string to evalute it lenght
  *
@@ -11,4 +11,7 @@
 int _strlen_recursion(char *s)
 {
 
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
