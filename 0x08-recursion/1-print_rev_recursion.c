472978e59ce4137677b2fe9f4ba5b25e30d72160
mwunnam <mwunnam@gmail.com>
2023-07-15 13:22:02 +0000

    done with task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion : prints a string followed by a newline
+ * _print_rev_recursion - prints a string followed by a newline
  *
  * @s: parameter for hold string to be reversed
  *
  * Return: 0 when successful
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
+	if (*s == '\0')
+		return; 
 
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
