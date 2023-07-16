fbb7ec12a14ab38ae9a1584876c144957c00f0f7
Michael Wunnam Issahaku <mwunnam@gmail.com>
2023-07-16 12:57:45 -0700

    done with task 5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that reutrns the x raised to the power y;
+ *_pow_recursion - function that reutrns the x raised to the power y;
  *
  * @x: base number
  * @y: expontial number
  *
- * return: 0
+ * return: result of powe
  */
 
-int  _pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
