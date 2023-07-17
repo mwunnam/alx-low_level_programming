478fd23a460564f0dbf89117b7fd1ec4359d9120
Michael Wunnam Issahaku <mwunnam@gmail.com>
2023-07-17 15:28:03 -0700

    done with task 0 and readme file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+
+
+/**
+ * struct dog - short description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Longer description
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
