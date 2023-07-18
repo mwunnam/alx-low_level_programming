4124956283ee51059db5edd11371eb080f579127
Michael Wunnam Issahaku <mwunnam@gmail.com>
2023-07-17 19:00:20 -0700

    done with all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,14 @@
-#ifndef DOG_STRUCT_H
-#define DOG_STRUCT_H
-
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - basic info of a do
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
  * Description: Longer description
- *
  */
-
 struct dog
 {
 	char *name;
@@ -20,9 +17,16 @@ struct dog
 };
 
 /**
-* dog_t - typedef for strut dog
-*/
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#endif /* DOG_STRUCT_H */
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
+
