7130fae0ac6c2de9ba04f04f885b72a664090101
Michael Wunnam Issahaku <mwunnam@gmail.com>
2023-07-17 18:15:09 -0700

    correction on task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,7 @@ struct dog
 /**
 * dog_t - typedef for strut dog
 */
-typedef stuct dog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_STRUCT_H */
