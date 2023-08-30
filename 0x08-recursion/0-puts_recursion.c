102f2790aeb1a8a93d11af8f2fe7524b913e2bf0
mwunnam <mwunnam@gmail.com>
2023-08-30 18:26:53 +0100

    done with question task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+* void _puts-recursion - Entry point
+*
+* @s: string to be printed
+*
+* Return: 0 (success)
+*
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* functions */
+void _puts_recursion(char *s);
+int _putchar(char s);
+
+
+
+
+#endif 
