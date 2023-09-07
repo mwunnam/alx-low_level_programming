2dafea5fd19573b08a9ed3f503602b24c3585c95
mwunnam <mwunnam@gmail.com>
2023-09-07 02:07:37 +0100

    done with task two

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* @argc: arguement count
+* @argv: arguement array
+*
+* Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+return (0);
+}
