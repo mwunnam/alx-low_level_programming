210becee802f38900dcfd42851e6ccf709808d23
mwunnam <mwunnam@gmail.com>
2023-09-07 01:57:19 +0100

    done with task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* @argc: arguement count
+* @argv: arguement array
+*
+* Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+return (0);
+}
