3e5f9c389fbbfd381d9318e88a55d1cfafe55049
mwunnam <mwunnam@gmail.com>
2023-09-11 17:09:56 +0100

    done with task o

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * struct dog - infomation
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Long description
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
