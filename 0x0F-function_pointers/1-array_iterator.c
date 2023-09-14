ca6af75062a988dbd33edc070d922727f07b84c0
mwunnam <mwunnam@gmail.com>
2023-09-14 05:35:18 +0000

    done with task 1 of function_pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* array_iterator - function that print array
+*
+* @array: array
+* @size: size of array
+* @action: action to be done
+*
+* Return: Void
+*
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+
+}
