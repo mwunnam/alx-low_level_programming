6aa5b0fdc6f8d9c157be6190e8d7a1bd3a28ed86
mwunnam <mwunnam@gmail.com>
2023-09-15 01:42:20 +0000

    correction on task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, const char *);
+		str = va_arg(args, char*);
 		if (str == NULL)
 			printf("nill");
 
