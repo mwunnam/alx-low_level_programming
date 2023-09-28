e2716a38f88475db9e9b0f7f68d3bc2f8b039ebd
mwunnam <mwunnam@gmail.com>
2023-09-28 22:30:25 +0000

    done with task 0 of bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* binary_to_uint - Function that convert binary to decimals
+*
+* @b: the binary to be converted
+*
+* Return: result when successful
+*/
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result;
+
+	result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	if (*b != '1' && *b != '0')
+		return (0);
+
+	while (*b != '\0')
+	{
+		result = (result << 1) | (*b - '0');
+		b++;
+	}
+return (result);
+}
