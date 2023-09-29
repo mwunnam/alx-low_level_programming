51bc2cca7d028222cf1cee121926661493b33c92
mwunnam <mwunnam@gmail.com>
2023-09-29 01:18:29 +0000

    done with task 4 of bit manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* clear_bit - Function that insert 1 at an index
+*
+* @n: number of the
+* @index: index to be inserted
+*
+* Return: 1 (success)
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit_number;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	bit_number = ~(1UL << index);
+
+	*n = *n & bit_number;
+
+return (1);
+}
