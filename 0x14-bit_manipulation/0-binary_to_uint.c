a3bdc92e3db13dc5ab1b93c9b5b4fc09b6aa64cb
mwunnam <mwunnam@gmail.com>
2023-09-29 00:11:25 +0000

    done with task 1 of bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,8 +18,6 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	if (*b != '1' && *b != '0')
-		return (0);
 
 	while (*b != '\0')
 	{
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_binary - Function prints binary numbers
+*
+* @n: number to be printed in binary
+*
+* Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+	int bit_pos;
+	int bit_print;
+	int bit;
+
+	bit_print = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	/**	_putchar('\n'); */
+		return;
+	}
+
+	bit_pos = sizeof(n) * 8 - 1;
+
+	while (!((n >> bit_pos) & 1))
+		bit_pos--;
+
+	for (; bit_pos >= 0; bit_pos--)
+	{
+		bit = (n >> bit_pos) & 1;
+
+		if (bit)
+		{
+			bit_print = 1;
+			_putchar('1');
+		}
+		else if (bit_print)
+		{
+			_putchar('0');
+		}
+	}
+/**	_putchar('\n'); */
+}
