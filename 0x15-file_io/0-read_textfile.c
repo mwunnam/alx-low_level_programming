a7cb60f8a505d3aa32ef50f1763ea123b48fe0f4
mwunnam <mwunnam@gmail.com>
2023-10-08 22:49:07 +0000

    done with task on of file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,55 @@
 #include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
-* main - Entry point
+* read_textfile - function that read and write into a file
 *
+* @filename: Name of file
+* @letters: number of letter to be printed
 * Return: 0 (success)
-*
 */
-int main(void)
+
+
+size_t read_textfile(const char *filename, size_t letters)
 {
-return (0);
+	FILE *ptr_file;
+	char *buffer;
+	ssize_t read;
+	ssize_t written;
+
+	if (filename == NULL)
+		return (0);
+
+	ptr_file = fopen("filename", "r");
+	if (ptr_file == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		fclose(ptr_file);
+		return (0);
+	}
+
+	read = fread(buffer, 1, letters, ptr_file);
+	if (read <= 0)
+	{
+		free(buffer);
+		fclose(ptr_file);
+		return (0);
+	}
+
+	written = write(STDOUT_FILENO, buffer, read);
+	if (written < 0 || written < read)
+	{
+		free(buffer);
+		fclose(ptr_file);
+		return (0);
+	}
+
+	free(buffer);
+	fclose(ptr_file);
+return (written);
 }
