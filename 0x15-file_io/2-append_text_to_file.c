e3880abf5e692b7fb1102d7ff63592c28ebe8ce8
mwunnam <mwunnam@gmail.com>
2023-10-09 22:39:33 +0000

    done with task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
+* append_text_to_file - Entry point
 *
-* Return: 0 (success)
+* @filename: Name of the file
+* @text_content: content to be appended
 *
+* Return: 1 (success)
 */
-int main(void)
+
+
+int append_text_to_file(const char *filename, char *text_content)
 {
-return (0);
+	FILE *file;
+	int result;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	file = fopen(filename, "a");
+	if (file == NULL)
+	{
+		fclose(file);
+		return (-1);
+	}
+
+	result = fputs(text_content, file);
+	if (result == EOF)
+	{
+		fclose(file);
+		return (-1);
+	}
+	fclose(file);
+return (1);
 }
