9e6dbbf7f8752973261edee77284dffde89118e6
mwunnam <mwunnam@gmail.com>
2023-10-09 11:51:03 +0000

    still correction on task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -52,7 +52,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	written = write(file, text_content, text_len);
-	if (written == -1)
+	if (written == -1 || file == -1)
 	{
 		close(file);
 		return (-1);
