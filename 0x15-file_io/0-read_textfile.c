b03cd78ab99015119e50748ded396f93ea9bce1f
mwunnam <mwunnam@gmail.com>
2023-10-31 19:23:11 +0000

    done with task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+* read_textfile - Function that reads the text of a file
+* @filename: Name of file who's content is to be read
+* @letters: Number of charaters to be read
+* Return: Returns the number of charactes read
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *ptr;
+	size_t byte_read;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	ptr = fopen(filename, "r");
+	if (ptr == NULL)
+	{
+		fclose(ptr);
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		fclose(ptr);
+		return (0);
+	}
+
+	byte_read = fread(buffer, 1, letters, ptr);
+
+	if (byte_read > 0)
+	{
+		fwrite(buffer, 1, byte_read, stdout);
+	}
+fclose(ptr);
+free(buffer);
+
+return (byte_read);
+}
