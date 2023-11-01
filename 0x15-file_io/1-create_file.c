e842fc7061809867b7b6eed41b7a8324666fbdf2
mwunnam <mwunnam@gmail.com>
2023-11-01 02:19:09 +0000

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 	}
 
-	byte_written = write(fd, text_content,(size_t)_strlen(text_content));
+	byte_written = write(fd, text_content, (size_t) _strlen(text_content));
 	if (byte_written == -1)
 	{
 		close(fd);
