6d9e554706a40dcdb83d0884732ccab383765beb
mwunnam <mwunnam@gmail.com>
2023-12-11 14:56:20 +0000

    done with task 2

---
+++ 0x18-dynamic_libraries/maths.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Function that add to numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: num1 + num2
+ **/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+/**
+ * sub - Function that add to numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: num1 - num2
+ **/
+int sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+/**
+ * mul - Function that add to numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: num1 * num2
+ **/
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * mod - Function that add to numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: num1 % num2
+ **/
+int mod(int num1, int num2)
+{
+	return (num1 % num2);
+}
+
+/**
+ * div - Function that add to numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: num1 % num2
+ **/
+int div(int num1, int num2)
+{
+	return (num1 / num2);
+}
+
