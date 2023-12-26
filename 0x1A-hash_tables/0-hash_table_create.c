45895086de0b0b16adc8b7de2bdbf81927a026ed
mwunnam <mwunnam@gmail.com>
2023-12-26 17:37:36 +0000

    correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,6 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
+
 	new_table->size = size;
 
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
@@ -33,5 +34,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		new_table->array[i] = NULL;
 	}
-	return (new_table);
+return (new_table);
 }
