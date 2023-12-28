c25d58539e7502797256b77fe3358b6bd3742a9c
mwunnam <mwunnam@gmail.com>
2023-12-28 23:27:13 +0000

    correction 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,12 +1,32 @@
 #include "lists.h"
 /**
+ * count_len - Function the count the lent of a list
+ * @h: The head of the list
+ * Return: count
+ **/
+unsigned int count_len(dlistint_t **h)
+{
+	dlistint_t *current;
+	unsigned int count;
+
+	count = 0;
+
+	current = *h;
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
+/**
  * insert_dnodeint_at_index - Function that insert node at index
  * @h: The head of the list
  * @idx: The index for insertion
  * @n: The value to be inserted
  * Return: new_node
  **/
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current1, *current2, *new_node;
@@ -16,11 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	count = 0;
 	current1 = *h;
-	while (current1 != NULL)
-	{
-		current1 = current1->next;
-		count++;
-	}
+	count = count_len(h);
 	if (count < idx)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
@@ -29,8 +45,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	current1 = *h;
 	if (idx == 0)
+	{
 		add_dnodeint(h, n);
-
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+	}
 	else if (idx == count)
 		add_dnodeint_end(h, n);
 	else
@@ -39,7 +59,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		for (count = 1; count < idx; count++)
 		{
 			current1 = current1->next;
-			current2 = current2->next;
+			current2 = current1->next;
 		}
 		new_node->prev = current1;
 		new_node->next = current2;
@@ -48,3 +68,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	return (new_node);
 }
+
