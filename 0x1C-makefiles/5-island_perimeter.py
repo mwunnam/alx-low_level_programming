3406787df50ec91964333ebdaa3edf1de7df8203
mwunnam <mwunnam@gmail.com>
2024-03-01 01:20:17 +0000

    done with correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -25,10 +25,10 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 size += 1
-                if (j > 0 and grid[i][j - 1] == 0):
+                if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
 
-                if (i > 0 and grid[i - 1][j] == 0):
+                if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
 
     return size * 4 - edges * 2
