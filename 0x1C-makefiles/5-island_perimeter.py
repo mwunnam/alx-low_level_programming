7db3a085e87f30559c0e83738128899b6703b338
mwunnam <mwunnam@gmail.com>
2024-03-01 00:42:28 +0000

    correction on task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,13 +21,20 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                # checking the top
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+
+                # checking left side
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j+1] == 0:
+
+                # check right side
+                if j == len(grid[0]) - 1 or grid[i][j+1] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+
+                # check bottom side
+                if i == len(gird) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
 
     return perimeter
